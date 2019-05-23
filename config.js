164e371ed1839bdc53fcb7f712af5916b7c6d5b6
Rafał Kuchta <rafalmax12@interia.pl>
2019-05-23 23:24:31 +0200

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://Rafal:tcppbwYRSZlSbRz5@cluster0-vym6g.mongodb.net/test?retryWrites=true',
+    keySession: ['Twoj klucz'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
